ed341ff753c2dc7cb13e46ed239c1c59e6817bcf
wanhaiyu <h.wan@wustl.edu>
2021-02-26 02:06:45 -0600

    first commit

--- src/main/java/com/twilio/Webapp.java
+++ src/main/java/com/twilio/Webapp.java
@@ -2,12 +2,12 @@ package com.twilio;
 
 import com.twilio.jwt.accesstoken.AccessToken;
 import com.twilio.jwt.accesstoken.VoiceGrant;
-import com.twilio.type.*;
+import com.twilio.twiml.voice.*;
 import com.twilio.twiml.voice.Client;
-import com.twilio.twiml.voice.Dial;
 import com.twilio.twiml.voice.Number;
+import com.twilio.twiml.voice.Record;
+import com.twilio.type.*;
 import com.twilio.twiml.VoiceResponse;
-import com.twilio.twiml.voice.Say;
 import com.twilio.twiml.TwiMLException;
 import com.twilio.http.TwilioRestClient;
 import com.twilio.http.HttpMethod;
@@ -23,6 +23,7 @@ import static spark.Spark.afterAfter;
 import static spark.Spark.get;
 import static spark.Spark.post;
 
+//import com.twilio.type.Client;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URLEncodedUtils;
 
@@ -36,9 +37,10 @@ import java.lang.Character;
 public class Webapp {
 
     static final String IDENTITY = "alice";
+    static final String to = "alice";
     static final String CALLER_ID = "client:quick_start";
     // Use a valid Twilio number by adding to your account via https://www.twilio.com/console/phone-numbers/verified
-    static final String CALLER_NUMBER = "1234567890";
+    static final String CALLER_NUMBER = "3144899693";
 
     public static void main(String[] args) throws Exception {
         // Load the .env file into environment
@@ -94,10 +96,17 @@ public class Webapp {
          *
          * @returns The TwiMl used to respond to an outgoing call
          */
+//        get("/makeCall", (request, response) -> {
+//            final String to = request.queryParams("to");
+//            System.out.println(to);
+//            return call(to);
+//        });
         get("/makeCall", (request, response) -> {
-            final String to = request.queryParams("to");
+            //final String to = request.queryParams("to");
+            final String to = "alice";
             System.out.println(to);
-            return call(to);
+            return mainMenu();
+            //return call(to);
         });
 
         /**
@@ -111,17 +120,31 @@ public class Webapp {
          *
          * @returns The TwiMl used to respond to an outgoing call
          */
+//        post("/makeCall", (request, response) -> {
+//            String to = "";
+//            List<NameValuePair> pairs = URLEncodedUtils.parse(request.body(), Charset.defaultCharset());
+//            Map<String, String> params = toMap(pairs);
+//            try {
+//                to = params.get("to");
+//            } catch (Exception e) {
+//                return "Error: " + e.getMessage();
+//            }
+//            System.out.println(to);
+//            return call(to);
+//        });
         post("/makeCall", (request, response) -> {
-            String to = "";
-            List<NameValuePair> pairs = URLEncodedUtils.parse(request.body(), Charset.defaultCharset());
-            Map<String, String> params = toMap(pairs);
-            try {
-                to = params.get("to");
-            } catch (Exception e) {
-                return "Error: " + e.getMessage();
-            }
+//            String to = "";
+//            List<NameValuePair> pairs = URLEncodedUtils.parse(request.body(), Charset.defaultCharset());
+//            Map<String, String> params = toMap(pairs);
+//            try {
+//                to = params.get("to");
+//            } catch (Exception e) {
+//                return "Error: " + e.getMessage();
+//            }
+            String to = "alice";
             System.out.println(to);
-            return call(to);
+            return mainMenu();
+            //return call(to);
         });
 
         /**
@@ -170,7 +193,202 @@ public class Webapp {
         post("/incoming", (request, response) -> {
             return greet();
         });
+        //TODO:: handle unanswered call
+        get("/handle-unanswered-call", (request, response) -> {
+            String status = request.queryParams("DialCallStatus");
+            System.out.println("didCallStatus: "+ status);
+            if(status.equals("busy")||status.equals("no-answer")){
+                return voicemail();
+            }
+            return new VoiceResponse.Builder().hangup(new Hangup.Builder().build()).build().toXml();
+        });
+        post("/handle-unanswered-call", (request, response) -> {
+//            String status = "";
+//            List<NameValuePair> pairs = URLEncodedUtils.parse(request.body(), Charset.defaultCharset());
+//            Map<String, String> params = toMap(pairs);
+//            try {
+//                status = params.get("DialCallStatus");
+//            } catch (Exception e) {
+//                return "Error: " + e.getMessage();
+//            }
+            String status = request.queryParams("DialCallStatus");
+            System.out.println("didCallStatus: "+ status);
+            if(status.equals("busy")||status.equals("no-answer")){
+                return voicemail();
+            }
+            return new VoiceResponse.Builder().hangup(new Hangup.Builder().build()).build().toXml();
+        });
+        //TODO::handle record voice mail
+        get("/handle-record-voicemail",(request, response) -> {
+            String recordingUrl = request.queryParams("RecordingUrl") +".mp3";
+            String transcriptionUrl = request.queryParams("TranscriptionUrl");
+            System.out.println("recordingUrl: "+ recordingUrl);
+            System.out.println("transcriptionUrl: "+ recordingUrl);
+            System.out.println("TEXT: "+request.queryParams("TranscriptionText"));
+            Say say = new Say.Builder("Congratulations! You have leave your message").build();
+            return new VoiceResponse.Builder().say(say).hangup(new Hangup.Builder().build()).build().toXml();
+
+        });
+        post("/handle-record-voicemail",(request, response) -> {
+            String recordingUrl = "";
+            String transciptionUrl = "" ;
+            String text = "";
+            List<NameValuePair> pairs = URLEncodedUtils.parse(request.body(), Charset.defaultCharset());
+            Map<String, String> params = toMap(pairs);
+            try {
+                recordingUrl = params.get("RecordingUrl")+".mp3";
+                transciptionUrl = params.get("TranscriptionUrl");
+                text = params.get("TranscriptionText");
+            } catch (Exception e) {
+                return "Error: " + e.getMessage();
+            }
+            System.out.println("recordingUrl: "+ recordingUrl);
+            System.out.println("transcriptionUrl: "+ transciptionUrl);
+            System.out.println("TEXT: "+text);
+            Say say = new Say.Builder("Congratulations! You have leave your message").build();
+            return new VoiceResponse.Builder().say(say).hangup(new Hangup.Builder().build()).build().toXml();
+        });
+        //TODO::option
+        get("/option",(request, response)->{
+            String selectedOption = request.queryParams("Digits");
+            System.out.println("The digits is: " + selectedOption);
+            String toXml;
+            switch (selectedOption){
+                case "1":
+                    toXml = call(to) ;
+                    break;
+                case "2":
+                    toXml= recordCall(to);
+                    break;
+                case "3":
+                    toXml = forwardingCall(to);
+                    break;
+                case "4":
+                    toXml = conference();
+                    break;
+                default:
+                    toXml= call(to);
+            }
+            return toXml;
+        });
+        post("/option", (request, response) -> {
+            String selectedOption = "";
+            List<NameValuePair> pairs = URLEncodedUtils.parse(request.body(), Charset.defaultCharset());
+            Map<String, String> params = toMap(pairs);
+            try {
+                selectedOption = params.get("Digits");
+            } catch (Exception e) {
+                return "Error: " + e.getMessage();
+            }
+            System.out.println("The digits is: " + selectedOption);
+            String toXml;
+
+            switch (selectedOption){
+                case "1":
+                    toXml = call(to) ;
+                    break;
+                case "2":
+                    toXml= recordCall(to);
+                    break;
+                case "3":
+                    toXml = forwardingCall(to);
+                    break;
+                case "4":
+                    toXml = conference();
+                    break;
+                default:
+                    toXml= call(to);
+            }
+            return toXml;
+        });
+        //TODO::handle recoding call
+        get("/handle-recording-call", (request, response) -> {
+            String recordingUrl = request.queryParams("RecordingUrl");
+            System.out.println("recordingUrl: "+ recordingUrl);
+
+            return recordingUrl;
+        });
+        post("/handle-recording-call", (request, response) -> {
+            String recordingUrl = request.queryParams("RecordingUrl");
+            System.out.println("recordingUrl: "+ recordingUrl);
+            return recordingUrl;
+        });
+        //TODO::forward call
+        get("/forward-call", (request, response) -> {
+            VoiceResponse voiceResponse;
+            Client client = new Client.Builder("bob").build();
+            Dial dial = new Dial.Builder().callerId(CALLER_ID).client(client)
+                    .timeout(10)
+                    .build();
+            voiceResponse = new VoiceResponse.Builder().dial(dial).build();
+            return voiceResponse.toXml();
+        });
+        post("/forward-call", (request, response) -> {
+            VoiceResponse voiceResponse;
+            Client client = new Client.Builder("bob").build();
+            Dial dial = new Dial.Builder().callerId(CALLER_ID).client(client)
+                    .timeout(10)
+                    .build();
+            voiceResponse = new VoiceResponse.Builder().dial(dial).build();
+            return voiceResponse.toXml();
+        });
     }
+    //TODO conference()
+    private static String conference(){
+        VoiceResponse response;
+        Conference conference = new Conference.Builder("Room 1234").build();
+        Dial dial = new Dial.Builder().conference(conference).build();
+        Say say = new Say.Builder("Welcome to the room 1234").build();
+        response = new VoiceResponse.Builder().dial(dial).build();
+        return response.toXml();
+    }
+    //TODO:: forwardIngCall 1 step
+    private static String forwardingCall(String to){
+        VoiceResponse voiceResponse;
+        Client client = new Client.Builder(to).build();
+        Dial dial = new Dial.Builder().callerId(CALLER_ID).client(client)
+                .timeout(10)
+                .action("/forward-call")
+                .build();
+        voiceResponse = new VoiceResponse.Builder().dial(dial).build();
+        return voiceResponse.toXml();
+    }
+    //TODO::make recording call
+    private static String recordCall(String to){
+        VoiceResponse voiceResponse;
+        Client client = new Client.Builder(to).build();
+        Dial dial = new Dial.Builder().callerId(CALLER_ID).client(client)
+                .timeout(10)
+                .action("/handle-unanswered-call")
+                .method(HttpMethod.GET)
+                .record(Dial.Record.RECORD_FROM_RINGING_DUAL)
+                .recordingStatusCallback("/handle-recording-call")
+                .build();
+        voiceResponse = new VoiceResponse.Builder().dial(dial).build();
+        return voiceResponse.toXml();
+    }
+    //TODO:: voicemail and make call implement
+    private static String voicemail(){
+        Say say = new Say
+                .Builder("Please leave a message at the beep.\nPress the star key when finished.").build();
+        Record record = new Record.Builder()
+                .maxLength(20).finishOnKey("*")
+                .transcribe(true)
+                .transcribeCallback("/handle-record-voicemail")
+                .build();
+        Say say2 = new Say.Builder("I did not receive a recording").build();
+        VoiceResponse response = new VoiceResponse.Builder().say(say)
+                .record(record).say(say2).hangup(new Hangup.Builder().build()).build();
+        return response.toXml();
+    }
+//    private static String sendVoicemail(){
+//        Say say = new Say.Builder("Congratulations! You have sent the voice mail.").build();
+//        return new VoiceResponse.Builder().say(say).build().toXml();
+//    }
+//    public static String makeCall(){
+//        Say say = new Say.Builder("Congratulations! You have made a phone call.").build();
+//        return new VoiceResponse.Builder().say(say).build().toXml();
+//    }
 
     private static String getAccessToken(final String identity) {
         // Create Voice grant
@@ -188,6 +406,19 @@ public class Webapp {
         return token.toJwt();
     }
 
+
+    //TODO:: main menu
+    private static String mainMenu(){
+        VoiceResponse voiceResponse;
+        voiceResponse = new VoiceResponse.Builder()
+                .gather(new Gather.Builder()
+                        .numDigits(1)
+                        .action("/option")
+                        .say(new Say.Builder("For regular call, press 1. For recording call, press 2.").build())
+                        .build()).build();
+        System.out.println("main menu");
+        return voiceResponse.toXml();
+    }
     private static String call(final String to) {
         VoiceResponse voiceResponse;
         String toXml = null;
@@ -197,11 +428,21 @@ public class Webapp {
         } else if (isPhoneNumber(to)) {
             Number number = new Number.Builder(to).build();
             Dial dial = new Dial.Builder().callerId(CALLER_NUMBER).number(number)
+                    .timeout(10)
+                    .action("/handle-unanswered-call")
+                    .method(HttpMethod.GET)
                     .build();
             voiceResponse = new VoiceResponse.Builder().dial(dial).build();
-        } else {
+        }else if(to.equals("music")){
+            Play play = new Play.Builder("https://api.twilio.com/cowbell.mp3").loop(0).build();
+            voiceResponse = new VoiceResponse.Builder().play(play).build();
+        }
+        else {
             Client client = new Client.Builder(to).build();
             Dial dial = new Dial.Builder().callerId(CALLER_ID).client(client)
+                    .timeout(10)
+                    .action("/handle-unanswered-call")
+                    .method(HttpMethod.GET)
                     .build();
             voiceResponse = new VoiceResponse.Builder().dial(dial).build();
         }
